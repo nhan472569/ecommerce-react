abf88f206dbe4ed433d71a2e0b377568fc79cd5b
nhan472569 <nhan472569@gmail.com>
2022-12-30 22:39:42 +0700

    complete cart & checkout page

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -4,15 +4,23 @@ import CartItem from './CartItem';
 
 const Cart = props => {
   const cart = useSelector(state => state.cart);
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  const date = new Date(Date.now());
   const total = cart.items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
   const quantity = cart.items.reduce((acc, item) => acc + item.quantity, 0);
   return (
-    <div className="container">
+    <div className="container flex-between">
       <div className={classes.cart}>
-        <h1 className={classes.title}>Giỏ hàng</h1>
+        <h2 className={classes.title}>Giỏ hàng</h2>
         {cart.items.map(item => (
           <CartItem
             key={item._id}
@@ -25,36 +33,55 @@ const Cart = props => {
           />
         ))}
       </div>
-      {/* <table className={classes.table}>
-        <thead>
-          <tr className={classes.head}>
-            <th>Tên sản phẩm</th>
-            <th>Giá</th>
-            <th>Số lượng</th>
-            <th>Thành tiền</th>
-            <th>Thao tác</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cart.items.map(item => (
-            <CartItem
-              key={item._id}
-              productId={item._id}
-              name={item.name}
-              price={item.price}
-              quantity={item.quantity}
-              imageCover={item.imageCover}
-            />
-          ))}
-        </tbody>
-      </table>
-      <div className={classes.summary}>
-        <div className={classes.total}>
-          <span>Tổng thanh toán ({quantity} sản phẩm): </span>
-          <span>{total.toLocaleString('vi-VN')} ₫</span>
+      <div className={classes.bill}>
+        <p className={classes['bill-title']}>Giao hàng</p>
+        <span className={classes['delivery-select']}>
+          <span>Miễn phí</span>
+          <span>Giao nhanh: {(29000).toLocaleString('vi-VN')} ₫</span>
+        </span>
+        <p className={classes['text-gray']}>
+          Ngày giao hàng: {date.toLocaleString('vi-VN', options)}
+        </p>
+        <hr></hr>
+        <form className={classes['discount-form']}>
+          <input
+            typeof="text"
+            placeholder="Mã khuyến mãi"
+            name="discount"
+            id="discount"
+            required
+          ></input>
+          <button type="submit">Áp dụng</button>
+        </form>
+        <hr></hr>
+        <div className="row flex-between mb-05">
+          <span className={classes['text-gray']}>Tạm tính</span>
+          <span className={classes['text-gray']}>
+            {total.toLocaleString('vi-VN')} ₫
+          </span>
         </div>
-        <button className={classes.checkout}>Thanh toán</button> */}
-      {/* </div> */}
+        <div className="row flex-between mb-05">
+          <span className={classes['text-light-gray']}>Giảm giá</span>
+          <span className={classes['text-light-gray']}>-0 ₫</span>
+        </div>
+        <div className="row flex-between mb-05">
+          <span className={classes['text-light-gray']}>Phí giao hàng</span>
+          <span className={classes['text-light-gray']}>0 ₫</span>
+        </div>
+        <div className="row flex-between mb-05">
+          <span className={classes['text-light-gray']}>Thuế</span>
+          <span className={classes['text-light-gray']}>+0 ₫</span>
+        </div>
+        <hr></hr>
+        <div className="row flex-between mb-05">
+          <span className={classes['text-gray']}>Tổng cộng</span>
+          <span className={classes['text-black']}>
+            {total.toLocaleString('vi-VN')} ₫
+          </span>
+        </div>
+        <button className={classes.checkout}>Thanh toán</button>
+        <button className={classes.continue}>Tiếp tục mua sắm</button>
+      </div>
     </div>
   );
 };
