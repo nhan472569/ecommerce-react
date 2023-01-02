4f4abfef64e416f7cae4e6d83d677395ac433415
nhan472569 <nhan472569@gmail.com>
2023-01-02 12:37:01 +0700

    remake 404 not found page

--- src/components/layout/NotFound.js
+++ src/components/layout/NotFound.js
@@ -1,10 +1,28 @@
-// import classes from './NotFound.module.css';
+import { Link } from 'react-router-dom';
+import classes from './NotFound.module.css';
 
 const NotFound = () => {
   return (
-    <div className="container">
-      <h1>404</h1>
-      <h2>Page not found</h2>
+    <div className="container position-relative d-flex flex-end">
+      <div className={classes.description}>
+        <h2>404</h2>
+        <p className={classes.subtitle}>
+          Oops!
+          <br /> Không tìm thấy trang
+        </p>
+        <p className={classes.explain}>
+          Trang bạn tìm kiếm không tồn tại hoặc đã bị gỡ!
+        </p>
+        <Link to="/home" className={classes.back}>
+          Quay trở lại trang chủ
+        </Link>
+      </div>
+      <div className={classes['notfound-img']}>
+        <img
+          src={process.env.PUBLIC_URL + '/images/404.jpg'}
+          alt="Not Found"
+        ></img>
+      </div>
     </div>
   );
 };
